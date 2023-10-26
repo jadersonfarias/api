dbb535b5fd2312684cc0092befb814a0fa835ddf
jaderson <jader375@gmail.com>
2023-10-26 00:02:12 -0300

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: "./src/server.js",
     instances: "max",
     env: {
-      NODE_ENV: "development"
+      NODE_ENV: "development",
     },
     env_production: {
       NODE_ENV: "production",
